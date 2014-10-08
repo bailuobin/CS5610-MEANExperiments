5093a246b75ee84eb9bf88bcec1a285b2426c78a
unknown <bai.lu@husky.neu.edu>
2014-10-07 23:49:28 -0400

    Angular Module

--- public/features/services/controller.js
+++ public/features/services/controller.js
@@ -1,57 +1,62 @@
-function ServicesCtrl($scope, $http){
-    console.log("Hello From ServiceController");
-    //$scope.message = "Hello World From Controller";
- 
-    $scope.renderServiceClients = function (response) {
-        $scope.serviceClients = response;
-    };
+app.controller(
+    "ApplicationsCtrl",  
+    function ApplicationsCtrl($scope, $http) {
+        console.log("Hello From ServiceController");
+        //$scope.message = "Hello World From Controller";
 
-    $scope.all = function () {
-        //get all
-        $http.get("/applications").success($scope.renderServiceClients);
-    };
-    
-    $scope.create = function () {
-        $http.post("/applications", $scope.currentServiceClient)
-        .success(function (response) {
-            $scope.all();
-        });       
-    };
+        $scope.renderServiceClients = function (response) {
+            $scope.serviceClients = response;
+        };
 
+        $scope.all = function () {
+            //get all
+            $http.get("/applications").success($scope.renderServiceClients);
+        };
 
-    $scope.remove = function (id) {
-        $http.delete("/applications/" + id)
-        .success(function (response) {
-            $scope.all();
-        });
-    };
+        $scope.create = function () {
+            $http.post("/applications", $scope.currentServiceClient)
+            .success(function (response) {
+                $scope.all();
+            });
+        };
 
 
-    $scope.select = function (id) {
-
-        console.log(id);
-
-        $http.get("/applications/" + id)
-        .success(function (response) {
-            $scope.currentServiceClient = response;
-            console.log(response);
-        });
-    };
-
-    $scope.update = function () {
-        $http.put("/applications/" + $scope.currentServiceClient._id, $scope.currentServiceClient)
-        .success(function (response) {
-            $scope.all();
-        });
-    };
+        $scope.remove = function (id) {
+            $http.delete("/applications/" + id)
+            .success(function (response) {
+                $scope.all();
+            });
+        };
 
 
+        $scope.select = function (id) {
 
+            console.log(id);
 
+            $http.get("/applications/" + id)
+            .success(function (response) {
+                $scope.currentServiceClient = response;
+                console.log(response);
+            });
+        };
+
+        $scope.update = function () {
+            $http.put("/applications/" + $scope.currentServiceClient._id, $scope.currentServiceClient)
+            .success(function (response) {
+                $scope.all();
+            });
+        };
 
 
 
 
 
-    $scope.all();
-}
+
+
+
+
+        $scope.all();
+
+
+    });
+
